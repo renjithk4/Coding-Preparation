8c0325449482ec4b24f8733b6a638947342ad39a
renjithk4 <renjithprasadk4@gmail.com>
2024-07-29 13:18:32 -0400

    stack sorting

---
+++ Recursion/stack_Sort.cpp
@@ -0,0 +1,31 @@
+
+void solve(stack<int> &sort, int element){
+    if((!sort.empty() && sort.top()<element) || sort.empty()){
+        sort.push(element);
+        return;
+    }
+
+    int n = sort.top();
+    sort.pop();
+
+    solve(sort,element);
+    sort.push(n);
+}
+void sorted(stack<int> &s){
+    if(s.empty()){
+       return;
+   }
+
+   int num = s.top();
+   s.pop();
+
+   sorted(s);
+
+   solve(s,num);
+}
+
+void SortedStack :: sort()
+{
+    sorted(s);
+
+}
